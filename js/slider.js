c6ae7ff90ab1106a4837fa72b02908e4c69f2cc6
Syed Mahad Shah Bukhari <110794379+mahadshahb@users.noreply.github.com>
2022-09-25 17:57:06 +0500

    work on taps-popup
    work on taps-popup

--- js/slider.js
+++ js/slider.js
@@ -78,3 +78,30 @@
 
         var nextBut = document.getElementById('next');
         nextBut.onclick = moveLeft;
+
+// ......popup...........//
+
+        function myFunction() {
+            var popup = document.getElementById("myPopup");
+            popup.classList.toggle("show");
+          }
+          function myFunction2() {
+            var popup2 = document.getElementById("myPopup2");
+            popup2.classList.toggle("show");
+          }
+          function myFunction3() {
+            var popup3 = document.getElementById("myPopup3");
+            popup3.classList.toggle("show");
+          }
+          function myFunction4() {
+            var popup4 = document.getElementById("myPopup4");
+            popup4.classList.toggle("show");
+          }
+          function myFunction5() {
+            var popup5 = document.getElementById("myPopup5");
+            popup5.classList.toggle("show");
+          }
+          function myFunction6() {
+            var popup6 = document.getElementById("myPopup6");
+            popup6.classList.toggle("show");
+          }
